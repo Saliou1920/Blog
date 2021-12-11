6f6dd9928e43332363cfe6e673ea4c49eba26dd6
Saliou <diopsaliou45@gmail.com>
2021-12-11 01:23:33 -0400

    adding bootstrap table to users

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,21 +1,36 @@
 <h2><?= $title; ?></h2><hr><br>
 <!-- loop on posts -->
 <div class="container">
-        <div class="row">
-            <?php foreach($users as $user) : ?>
-                <div class="col-lg-4 col-md-6 mb-3">
-                    <div class="card">
-                    <img class="card-img-top" style="height: 250px" src="" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $user['name']; ?></h5>
-                            <small class="post-date"><strong>Posted on:</strong> <?php echo $user['register_date']; ?></small><br>
-                            <a class="btn btn-info btn-sm" href="<?php echo site_url('/users/'.$user['id']); ?>">Read More</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Zipcode</th>
+                    <th scope="col">Register Date</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($users as $user) : ?>
+                    <tr>
+                        <th scope="row"><?php echo $user['id']; ?></th>
+                        <td><?php echo $user['name']; ?></td>
+                        <td><?php echo $user['username']; ?></td>
+                        <td><?php echo $user['email']; ?></td>
+                        <td><?php echo $user['zipcode']; ?></td>
+                        <td><?php echo $user['register_date']; ?></td>
+                        <td><a href="<?php echo site_url('users/edit/'.$user['id']); ?>" class="btn btn-primary" style="padding: 10px 15px;">Edit</a></td>
+                    <td><a href="<?php echo site_url('users/delete/'.$user['id']); ?>" class="btn btn-danger" style="padding: 10px 15px;">Delete</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
+</div>
 <!-- end loop -->
-
 
