198ea8dbda846fc473886f5cc0fbf73dedf15f26
Saliou <diopsaliou45@gmail.com>
2021-12-11 01:24:02 -0400

    update success view

--- application/views/users/success.php
+++ application/views/users/success.php
@@ -3,7 +3,7 @@
         <div class="col-md-12 text-white">
             <h1 style="font-size: 2rem">Success</h1>
             <p style="font-size: 1.5rem">You have successfully registered.</p>
-            <p><a class="btn btn-outline-success btn-sm" href="<?php echo base_url(); ?>">Home</a></p>
+            <p><a class="btn btn-outline-success btn-sm" href="<?php echo base_url().'users/'; ?>">Go to Users</a></p>
 
         </div>
     </div>
