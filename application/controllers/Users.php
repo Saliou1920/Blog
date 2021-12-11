4eba7d29b163e26fe123efd3cfad4f8978b0f208
Saliou <diopsaliou45@gmail.com>
2021-12-10 23:49:51 -0400

    adding edit and delete functions

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -16,6 +16,20 @@ class Users extends CI_Controller {
         $this->load->view('users/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function view($id = NULL)
+    {
+        $data['user'] = $this->user_model->get_users($id);
+        if (empty($data['user']))
+        {
+            show_404();
+        }
+        $data['title'] = $data['user']['name'];
+        $this->load->view('templates/header');
+        $this->load->view('users/view', $data);
+        $this->load->view('templates/footer');
+    }
+
 	public function create(){
         $this->load->helper('form');
         $this->load->library('form_validation');
@@ -48,4 +62,42 @@ class Users extends CI_Controller {
         }
     }
 	
+    public function edit($id = NULL)
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $data['user'] = $this->user_model->get_users($id);
+            if (empty($data['user']))
+            {
+                show_404();
+            }
+            $this->load->view('templates/header');
+            $this->load->view('users/edit', $data);
+            $this->load->view('templates/footer');
+
+        }
+        else
+        {
+            $this->user_model->update_user($id);
+            $this->load->view('templates/header');
+            $this->load->view('users/success');
+            $this->load->view('templates/footer');
+
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->user_model->delete_user($id);
+        $this->load->view('templates/header');
+        $this->load->view('users/success');
+        $this->load->view('templates/footer');
+    }
 }
